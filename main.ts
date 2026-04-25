423f850ae51e23bf117216b960c67a1e93d6bd98
utkarshbhargava1443k-jpg <utkarshbhargava1443k@gmail.com>
2026-04-24 22:12:46 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+player.onChat("run", function (num1, num2) {
+    agent.attack(FORWARD)
+    agent.collectAll()
+    agent.collect(DIAMOND_SWORD)
+})
+gameplay.setWeather(CLEAR)
+gameplay.setDifficulty(EASY)
+player.say(":)")
+blocks.place(CRAFTING_TABLE, pos(0, 0, 0))
+castles.buildSimpleCastle(COBBLESTONE)
+castles.buildCastleTower(
+84,
+42,
+COBBLESTONE
+)
